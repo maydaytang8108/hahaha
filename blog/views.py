9e8dd291be69979b132dc08e5d91ea9620aadc74
maydaytang8108 <34510147+maydaytang8108@users.noreply.github.com>
2018-04-11 19:17:39 +0800

    Add files via upload

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+
+def home_page(request):
+     return render(request, 'blog/home_page.html')
